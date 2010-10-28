2a18cc2b547d173341f3628780d512f1c9521baf
Jonathan <jonathan@impacti.org>
2010-10-28 19:11:01 +0100

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropboxchatapp::Application.config.secret_token = 'ccea482a718fbc3a55765b21c76462c76d7751c0676a7ec13839bf5985beca3a2c2742a541fea9dfccdb1a2a972846ef195c6e76336461a32610e2fae23d1f31'
